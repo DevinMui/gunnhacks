ed27ae0e091eacf933458397a1cd576fc12e82e5
Jesse Liang <jesseliang@ymail.com>
2016-11-11 21:25:31 -0800

    saem

--- glasses/glasses/ViewController.swift
+++ glasses/glasses/ViewController.swift
@@ -13,7 +13,10 @@ import Alamofire
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
     
-    var items = [[89.0, 123.1]]
+    
+    var list = [CLLocationCoordinate2DMake(20, 20), CLLocationCoordinate2DMake(10, 10)]
+
+    var url = "https://f21f5049.ngrok.io"
     
     var locationManager = CLLocationManager()
     
@@ -30,49 +33,60 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: 420, height: 60))
         self.view.addSubview(navBar);
         let navItem = UINavigationItem(title: "");
-        navBar.barTintColor = UIColor(red: 33/255, green: 150/255, blue: 243/255, alpha: 1.0)
+        navBar.barTintColor = UIColor(red: 33/255, green: 150/255, blue: 243/255, alpha: 0.9)
         navBar.setItems([navItem], animated: false);
 
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-        label.center = CGPoint(x: 50, y: 40)
+        label.center = CGPoint(x: 65, y: 40)
         label.textAlignment = .center
-        label.text = "Enstalk"
+        label.text = "Enstalk 2.0"
         label.font = UIFont(name: "HelveticaNeue", size: CGFloat(22))
         label.textColor = UIColor.white
         self.view.addSubview(label)
         
-        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+        Alamofire.request(url).responseJSON { response in
             
             if let JSON = response.result.value {
                 //print(JSON)
                 
             }
         }
-
         
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 1.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        var markers = [GMSMarker()]
+        
+        for i in 0..<list.count {
+            
+            markers.append(GMSMarker(position: list[i]))
+        }
+        
+        for i in 0..<list.count {
+            
+            print(markers[i])
+            
+            markers[i].title = "name"
+            markers[i].snippet = "Population: 8,174,100"
+            //marker1.icon = UIImage(named: "house")
+            markers[i].map = mapView
+        }
     }
-
     
-    override func loadView() {
+    
+
+    /*override func loadView() {
         
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
         view = mapView
         
-        let position = CLLocationCoordinate2DMake(10, 10)
-        let marker = GMSMarker(position: position)
-        marker.title = "name"
-        marker.snippet = "Population: 8,174,100"
-        //marker.icon = UIImage(named: "house")
-        marker.map = mapView
-        
-        let position1 = CLLocationCoordinate2DMake(20, 20)
-        let marker1 = GMSMarker(position: position1)
-        marker1.title = "name"
-        marker1.snippet = "Population: 8,174,100"
-        //marker1.icon = UIImage(named: "house")
-        marker1.map = mapView
 
-    }
+        
+
+        
+    }*/
 }
